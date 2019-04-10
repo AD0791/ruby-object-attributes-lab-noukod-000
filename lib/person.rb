d91be621a6b9fcbbcbd6872f7d74303556ab2423
Alexandro Disla <alexandrodisla@hotmail.com>
2019-04-10 03:41:33 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,17 @@
 class Person
+  def name = (name)
+    @person_name = name
+  end
   
+  def name
+    @person_name
+  end
+  
+  def job = (getjob)
+    @personjob = getjob
+  end
+  
+  def job
+    @personjob
+  end
 end
