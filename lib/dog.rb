e68a14e8e736ce19cca8ea73ba6728ac336f7279
Alexandro Disla <alexandrodisla@hotmail.com>
2019-04-10 03:39:22 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog 
+  def name = (dogname)
+    @dog_name = dogname
+  end
+  
+  def name
+    @dog_name
+  end
+  
+  def breed = (dogbreed)
+    @dogbreed = dogbreed
+  end
+  
+  def name
+    @dogbreed
+  end
+end  
